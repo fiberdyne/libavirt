cb2d64d351b22cf32254a0781634a107b01488f4
Mark Farrugia <mark.farrugia@fiberdyne.com.au>
2018-10-01 16:58:08 +1000

    Rename top-level header, fix CMakeLists target
    Signed-off-by: Mark Farrugia <mark.farrugia@fiberdyne.com.au>

--- src/avirt-config.c
+++ src/avirt-config.c
@@ -19,7 +19,9 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-#include "avirt-config.h"
+#define _GNU_SOURCE
+
+#include <avirt/avirt.h>
 
 #include <stdbool.h>
 #include <alsa/asoundlib.h>
