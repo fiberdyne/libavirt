90ab8dc3b45eb8638b92084225a8ce876e9f5342
Mark Farrugia <mark.farrugia@fiberdyne.com.au>
2018-10-11 12:34:46 +1100

    Update licensing
    Signed-off-by: Mark Farrugia <mark.farrugia@fiberdyne.com.au>

--- include/avirt/avirt.h
+++ include/avirt/avirt.h
@@ -1,22 +1,23 @@
 /*
- * AVIRT User-space library
+ * Application interface library for the AVIRT driver
  * 
- * avirt.h - AVIRT User-space library header
+ * avirt.h - AVIRT Application interface library header
  *
  * Copyright (C) 2018 Fiberdyne Systems Pty Ltd
+ * Author: Mark Farrugia <mark.farrugia@fiberdyne.com.au>
  *
- * This program is free software: you can redistribute it and/or modify
+ * This library is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 2 of the License, or
  * (at your option) any later version.
  *
- * This program is distributed in the hope that it will be useful,
+ * This library is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * along with this library.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 #ifndef _AVIRT_H_
